c433def627ab12c4ab6382ead83900fe8d664f18
Athos van Kralingen <athos.van.kralingen@hva.nl>
2016-03-20 20:02:19 +0100

    Added collider to paddle

--- Pong/Paddle/Paddle.cpp
+++ Pong/Paddle/Paddle.cpp
@@ -1,6 +1,8 @@
 #include <Artifact/Core/EntitySystem.h>
 #include <Artifact/Rendering/SpriteRenderer.h>
 #include <Artifact/IO/ResourceManager.h>
+#include <Artifact/Physics/BoxCollider2D.h>
+#include <Artifact/Physics/RigidBody2D.h>
 
 #include "Paddle.h"
 #include "PlayerInputComponent.h"
@@ -16,6 +18,12 @@ Paddle::Paddle(unsigned a_ID, Artifact::EntitySystem& a_EntitySystem)
     m_SpriteRenderer->setTexture(Artifact::ResourceManager::getTexture("Assets/Textures/Paddle.png"));
     m_SpriteRenderer->Width = 1.0f;
     m_SpriteRenderer->Height = 0.2f;
+
+    auto collider = addComponent<Artifact::BoxCollider2D>();
+    collider->setDimensions(glm::vec2(1.0f, 0.2f));
+    collider->enableTriggerState();
+    auto rigidBody = addComponent<Artifact::RigidBody2D>();
+    rigidBody->setGravityScale(0.0f);
 }
 
 void Paddle::setColor(Artifact::Color a_Color)
